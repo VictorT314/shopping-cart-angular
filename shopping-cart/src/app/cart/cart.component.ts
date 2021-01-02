219adb2a05396f2254d1ed356edf9d6f0d71f1bf
Victor Tavares <victor.as156@gmail.com>
2021-01-02 12:35:00 -0300

    Implementando formulário de checkout

--- shopping-cart/src/app/cart/cart.component.ts
+++ shopping-cart/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,12 +9,28 @@ import { CartService } from '../services/cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
+
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) { 
+    this.checkoutForm = this.formBuilder.group({
+      nome: '',
+      endereco: ''
+    });
+  }
 
   ngOnInit(){
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData) {
+    // Processamento dos dados de checkout
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Seu pedido foi realizado', customerData);
   }
 
 }
